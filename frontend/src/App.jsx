ce6ebb48112a189ba241da705c1e04e05c395e86
Vincent Bui <vincent.bui657@gmail.com>
2024-05-28 16:41:29 -0400

    Add react-router-dom and frontend pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AddNew from './pages/AddNew';
+import Show from './pages/Show';
+import Edit from './pages/Edit';
+import Delete from './pages/Delete';
 
 const App = () => {
   return (
-    <div className='bg-red-700 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='books/addnew' element={<AddNew />}/>
+      <Route path='books/details/:id' element={<Show />}/>
+      <Route path='books/edit/:id' element={<Edit />}/>
+      <Route path='books/delete/:id' element={<Delete />}/>
+    </Routes>
   )
 }
 
