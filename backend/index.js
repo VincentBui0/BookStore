e911f1d9c1a212da6cb040a660d6ac6c82ae6bb4
Vincent Bui <vincent.bui657@gmail.com>
2024-05-28 23:31:38 -0400

    Hide MongoDB string in .env

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import {PORT, mongoDBURL} from "./config.js";
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -30,7 +32,7 @@ app.get('/', (request, response) => {
 app.use('/books', booksRoute);
 
   mongoose
-  .connect(mongoDBURL)
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('App connected to database');
     app.listen(PORT, () => {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
       </div>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>Books List</h1>
-        <Link to='/books/create'>
+        <Link to='/books/addnew'>
           <MdOutlineAddBox className='text-sky-800 text-4xl' />
         </Link>
       </div>
